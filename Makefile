6a4b6ec61ae44670ba6f2d80ca0890ee790e730f
Suraj Nanavare <surajsnanavare@gmail.com>
2022-12-30 17:11:17 +0000

    Django setup

---
+++ Makefile
@@ -0,0 +1,36 @@
+include .env
+export
+
+verbosity=1
+
+help:
+	@echo "Usage:"
+	@echo " make help			-- display this help"
+	@echo " make Install			-- creates database, install dependancies and migrates migrations"
+	@echo " make run			-- run server"
+	@echo " make test			-- run the tests and checks lint issues"
+	@echo " make install-django		-- migrates migrations"
+	@echo " make install-pip		-- install dependancies"
+
+
+# Install dependancies and run migrations
+install: install-pip install-django
+
+# Install all required dependencies
+install-pip:
+	@pip install -r requirements.txt
+
+# Run migration on database
+install-django:
+	@python manage.py collectstatic  --no-input
+	@python manage.py migrate
+
+# Start application
+run:
+	@python manage.py runserver 0.0.0.0:${PORT}
+
+# Run tests
+test:
+	@flake8 .
+	@coverage run  --source=$(path) manage.py test --keepdb --verbosity=$(verbosity) $(path)
+	@coverage report -m
---
+++ core/settings.py
@@ -0,0 +1,127 @@
+import os
+import environ
+import dj_database_url
+from pathlib import Path
+
+env = environ.Env()
+# Reading .env file
+environ.Env.read_env(env_file='.env')
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY', default='django-secret-key')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(env('DEBUG', default=1))
+
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*').split(',')
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+# Register business applications
+BUSINESS_APPS = [
+    "authentication",
+    "users",
+    "images"
+]
+INSTALLED_APPS += BUSINESS_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL', default='sqlite:///db.sqlite3'),
+        conn_health_checks=True,
+    ),
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = env('STATIC_URL', default='static/')
+STATIC_ROOT = os.path.join(BASE_DIR, env('STATIC_ROOT', default='static'))
+
+# Base url to serve media files
+MEDIA_URL = env('MEDIA_URL', default='/media/')
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, env('MEDIA_ROOT', default='media/'))
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
